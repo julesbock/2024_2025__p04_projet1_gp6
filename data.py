8ae66e206be4a1611d861873ac0df09c29d019a9
adrilnd <barouxarbonaadrien@gmail.com>
2024-09-24 09:51:53 +0200

    truth
    I'm the truth teller

--- data.py
+++ data.py
@@ -1,12 +1,12 @@
 maxime = ("M" + "o" + "c" + "h" + "e")
 jules = ("M"+ "o" + "g" + "o" +"s" + "s" + "e")
-adrien = ("C" + "r" + "o" + "t" + "t" + "e " + "universelle")
+adrien = ("G" +"O"+"D")
 a=12
 
 a = 18 # et rien d'autre
 print (maxime)
 print (jules)
-print ("adrien est une " + adrien)
+print ("adrien est :  " + adrien)
 
 
 input("What is your name") #question essentielle du programme
