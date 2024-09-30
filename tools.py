503d0c3359c8ccd6668fa3f5cf94aceba0caa837
adrilnd <barouxarbonaadrien@gmail.com>
2024-09-30 10:34:02 +0200

    first tool def
    we must also define the different variable, the text can also put in a variable in data

--- tools.py
+++ tools.py
@@ -1,1 +1,11 @@
-# hi it s a tool file
+def ask_for_the_init_number ():
+    init_number_entry = input("Donnez le nombre que vous voulez transformer : ")
+    while init_number_entry_is_not_a_number:
+        try:
+            init_number_entry = input("Entrez un nombre entier : ")
+            init_number_int = int(init_number_entry)
+            return init_number_int
+        except ValueError:
+            print("Ce n'est pas un nombre entier valide. Veuillez réessayer.")
+ask_for_the_init_base = 
+ask_for_the_target_base = 
