85ec4183984f7561476ba1513dbb22c2b8815a46
MaxBOURG-Sav <maxbourg74@gmail.com>
2024-09-30 10:39:55 +0200

    update 1 data and tools

--- main_project_1.py
+++ main_project_1.py
@@ -3,7 +3,7 @@ def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
     target_number = None
     return target_number
 
-from utils import *
+from data_projet_1 import *
 
 assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 
--- tools.py
+++ tools.py
@@ -1,9 +1,11 @@
+from data_projet_1 import *
+
 def ask_for_the_init_number ():
     init_number_entry = input("Donnez le nombre que vous voulez transformer : ")
     while init_number_entry_is_not_a_number:
         try:
-            init_number_entry = input("Entrez un nombre entier : ")
-            init_number_int = int(init_number_entry)
+            init_number_entry 
+            init_number_int 
             return init_number_int
         except ValueError:
             print("Ce n'est pas un nombre entier valide. Veuillez réessayer.")
