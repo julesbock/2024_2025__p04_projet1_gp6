0d4fba5f09eaa1439b07457b9a2b3eddedca3a38
julesbock <julesbock08@icloud.com>
2024-09-30 11:04:04 +0200

    rge

--- data.py
+++ data.py
@@ -2,4 +2,4 @@ init_number_entry = input("Entrez un nombre entier : ")
 init_number_int = int(init_number_entry)
 init_number_entry_is_not_a_number = True
 
-init_base_entry = input ("Entrez la base d'origine du votre nombre")
+
