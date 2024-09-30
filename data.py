df6d40d1e4cb2bfe752b3cb84f327bfdcfccf2a7
adrilnd <barouxarbonaadrien@gmail.com>
2024-09-30 11:12:56 +0200

    data update for the base verification

--- data.py
+++ data.py
@@ -3,3 +3,7 @@ init_number_int = int(init_number_entry)
 init_number_entry_is_not_a_number = True
 
 init_base_entry = input ("Entrez la base d'origine du votre nombre")
+binary_list = ["2", "bin", "binary", "binaire"]
+hexa_list = ["16", "hexa", "hex", "hexadecimal", "hexadécimal"]
+decimal_list = ["10", "dec", "decimal", "décimal"]
+base_list = [binary_list, hexa_list, decimal_list]
