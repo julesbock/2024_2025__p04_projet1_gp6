3fbac8ca3a7a014a3b3e1c06be40aa7efd005ea7
julesbock <julesbock08@icloud.com>
2024-09-30 10:52:03 +0200

    variable changes

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-ask_for_the_init_number = 
-ask_for_the_init_base = 
-ask_for_the_target_base = 
+ask_for_init_number = 
+ask_for_init_base = 
+ask_for_target_base = 
 init_number_entry = input("Entrez un nombre entier : ")
 init_number_int = int(init_number_entry)
 init_number_entry_is_not_a_number = True
