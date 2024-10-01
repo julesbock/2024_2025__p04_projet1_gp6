e1a287b15352c948923c3c1351127185fcbde7e9
MaxBOURG-Sav <maxbourg74@gmail.com>
2024-10-01 09:37:51 +0200

    Update brouillon_Max.py

--- brouillon_Max.py
+++ brouillon_Max.py
@@ -1,9 +1,16 @@
 #Here,  the brouillon fichier
 from data import *
-def hex_to_bin (init_number_entry):
-    int(init_number_entry, 16)
+# def hex_to_bin (init_number_entry):
+#     int(init_number_entry, 16)
 
 
+def ask_for_init_base ():
+    init_base_entry
+    if check_base (init_base_entry):
+        return 
+    else:
+        print (text_error_base_entry)
+        ask_for_init_base ()
 
 
 def check_base (init_base_entry):
@@ -19,9 +26,10 @@ def check_base_bis (target_base_entry):
         return False
 
 def ask_for_target_base ():
-    target_base_entry #= input ("Entrez la base finale pour votre nombre")
-    if check_base (target_base_entry):
-        return target_base_entry
-    else:
-        print (text_error_base_entry)
-        ask_for_target_base ()
+    target_base_entry 
+    for target_base_entry in base_list:
+        if target_base_entry == base_list:
+            return target_base_entry
+        else: 
+            # print (text_error_base_entry)
+            ask_for_target_base ()
