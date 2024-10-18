f2e848348c5dc8af9b185f31c3ba653f6ca6fa93
MaxBOURG-Sav <maxbourg74@gmail.com>
2024-10-18 12:50:33 +0200

    hec_to_dec create

--- brouillon_Max.py
+++ brouillon_Max.py
@@ -1,24 +1,36 @@
-#Here,  the brouillon fichier
+# #Here,  the brouillon fichier
 from data import *
 from tools import *
-# def hex_to_bin (init_number_entry):
-#     int(init_number_entry, 16)
+# # def hex_to_bin (init_number_entry):
+# #     int(init_number_entry, 16)
 
 
-def dec_to_bin(init_number):#le init_number doit etre un int
-    if init_number is not int:      # faudra check les reponses imédiates : =0 et si le nombre n est pas négatif
-        raise ValueError ("Le nomnre doit être un entier")
-    if init_number < 0:
-        raise ValueError ("Le nombre est négatif, il doit être positif")
-    if init_number == 0:
-        return "0"
-    bin_number = ""
-    while init_number > 0:
-        number_conversion = init_number % 2
-        bin_number = str(number_conversion) + bin_number
-        init_number //= 2
-    return bin_number
+# def dec_to_bin(init_number):#le init_number doit etre un int
+#     if init_number is not int:      # faudra check les reponses imédiates : =0 et si le nombre n est pas négatif
+#         raise ValueError ("Le nomnre doit être un entier")
+#     if init_number < 0:
+#         raise ValueError ("Le nombre est négatif, il doit être positif")
+#     if init_number == 0:
+#         return "0"
+#     bin_number = ""
+#     while init_number > 0:
+#         number_conversion = init_number % 2
+#         bin_number = str(number_conversion) + bin_number
+#         init_number //= 2
+#     return bin_number
  
 
-def hex_to_dec (hex_number):
+def hex_to_dec (init_number):
     hex_list
+    decimal_number = 0
+    for i in range(len(init_number)):
+        list_result = hex_list[-(i+1)].lower()
+        decimal_number += hex_list.index(list_result) * (16**i)
+    return decimal_number
+
+
+
+init_number = "1ABDE369"
+decimal_value = hex_to_dec(init_number)
+# print(f"La valeur décimale de {init_number} est {decimal_value}.")
+# seulement pour les tests de la fct
