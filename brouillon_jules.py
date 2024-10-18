ec19fb8b13b6fa7cdc7ddb7c5fa39ad893d0b30b
julesbock <julesbock08@icloud.com>
2024-10-18 13:09:24 +0200

    Update brouillon_jules.py

--- brouillon_jules.py
+++ brouillon_jules.py
@@ -16,3 +16,27 @@ def dec_to_hex (init_number):
 
 
 print(dec_to_hex (1000))
+
+# data
+target_number = 0
+thank_you_text = "Merci d'avoir utilisé notre outil de conversion."
+restart_text = "Souhaitez vous convertir un autre nombre ?"
+goodbye_text = thank_you_text + "A bientot !"
+input_error_text = "Veuillez répondre par oui ou par non"
+
+#tools
+def ask_for_restart ():
+    answer = input(restart_text).lower()
+    while True:
+        if answer == "oui":
+            execute_convertion ()
+        elif answer == "non":
+            print(goodbye_text)
+        else:
+            print(input_error_text)
+
+
+def give_result ():
+    print (f"Le nombre converti est : {target_number}"
+           + "\n"
+           + thank_you_text)
