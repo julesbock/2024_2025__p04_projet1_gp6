20814989d5aabd9d812661345eded33e09e86234
MaxBOURG-Sav <maxbourg74@gmail.com>
2024-11-04 11:45:51 +0100

    suppression de 2 fcts inutiles dans main

--- main.py
+++ main.py
@@ -19,23 +19,3 @@ def ask_for_restart ():
             print(input_error_text)
 
 run_main()
-
-
-
-# def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
-#     pass
-#     target_number = None
-#     return target_number
-
-
-
-# assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
-
-# def do_the_job ():
-#     init_number = ask_for_init_number ()
-#     init_base = ask_for_init_base ()
-#     target_base = ask_for_target_base ()
-#     target_number = bin_dec_hex__to__bin_dec_hex (init_number, \
-#                                     init_base, \
-#                                     target_base)
-
